e5c99342b8f01c8073f0addb756f5c1fd2720b67
Madeline Ferguson <lusul101@gmail.com>
2020-10-24 19:45:55 -0700

    Work on endpoint queries

--- backend/express/routes/houses.js
+++ backend/express/routes/houses.js
@@ -41,23 +41,74 @@ router.get('/:houseId', async function(req, res) {
     res.json(house);
 });
 
-router.get('/:houseId/purchases', function(req, res) {
-    res.send('Get puchases for house ' + req.params['houseId']);
+router.get('/:houseId/purchases', async function(req, res) {
+    var houseId = req.params['houseId'];
+
+    var purchases = await knex.select('purchase_roommate', 
+        'purchase_amount', 'purchase_memo')
+        .from('purchases')
+        .join('roommates', 'roommate_id', '=', 'purchase_roommate')
+        .join('houses', 'house_id', '=', 'roommate_house')
+        .where('house_id', houseId);
+
+    res.json(purchases);
 });
 
-router.post('/:houseId/purchases', function(req, res) {
-    res.send('Add purchases to house ' + req.params['houseId']);
+router.post('/:houseId/purchases', async function(req, res) {
+    var roommate = req.body.roommate;
+    var amount = req.body.amount;
+    var memo = req.body.memo;
+
+    var id = await knex('purchases')
+        .insert({purchase_roommate: roommate, purchase_amount: amount, 
+        purchase_memo: memo});
+
+    res.json({id: id[0]});
 });
 
-router.get('/:houseId/purchases/:purchaseId', function(req, res) {
-    res.send('Get purchase ' + req.params['purchaseId'] + ' from house ' + req.params['houseId']);
+router.get('/:houseId/purchases/:purchaseId', async function(req, res) {
+    var purchaseId = req.params['purchaseId'];
+
+    var purchase = await knex.select('purchase_roommate', 
+        'purchase_amount', 'purchase_memo')
+        .from('purchases')
+        .where('purchase_id', purchaseId);
+
+    var divisions = await knex.select('division_id', 'division_amount', 
+        'division_memo', 'division_roommate_join_roommate')
+        .from('divisions')
+        .join('division_roommate_join', 'division_roommate_join_division', '=', 'division_id')
+        .where('division_purchase', purchaseId);
+
+    // var divisionsList = new Array();
+    
+    // divisions.forEach(async (division) => {
+    //     var roommatesList = new Array();
+    //     roommates.forEach(roommate => {
+    //         roommatesList.push(roommate['division_roommate_join_roommate']);
+    //     });
+
+    //     divisionsList.push({amount: division['division_amount'], 
+    //         roommates: roommatesList});
+    // });
+
+    res.json({roommate: purchase[0]['purchase_roommate'], 
+        amount: purchase[0]['purchase_amount'], 
+        // divisions: divisions, 
+        memo: purchase[0]['purchase_memo']});
 });
 
-router.delete('/:houseId/purchases/:purchaseId', function(req, res) {
-    res.send('Delete purchase ' + req.params['purchaseId'] + ' from house ' + req.params['houseId']);
+router.delete('/:houseId/purchases/:purchaseId', async function(req, res) {
+    const purchaseId = req.params['purchaseId'];
+
+    var rowsDeleted = await knex('purchases')
+        .where('purchase_id', purchaseId)
+        .del();
+        
+    res.json({'rows deleted': rowsDeleted});
 });
 
-router.patch('/:houseId/purchases/:purchaseId', function(req, res) {
+router.patch('/:houseId/purchases/:purchaseId', async function(req, res) {
     res.send('Patch purchase ' + req.params['purchaseId'] + ' from house ' + req.params['houseId']);
 });
 
@@ -65,15 +116,15 @@ router.post('/:houseId/purchases/:purchaseId/receipt', function(req, res) {
     res.send('Add receipt for purchase ' + req.params['purchaseId'] + ' from house ' + req.params['houseId']);
 });
 
-router.get('/:houseId/statistics/:roommateId', function(req, res) {
+router.get('/:houseId/statistics/:roommateId', async function(req, res) {
     res.send('Get statistics for roommate ' + req.params['roommateId'] + ' in house ' + req.params['houseId']);
 });
 
-router.get('/:houseId/debts/:roommateId', function(req, res) {
+router.get('/:houseId/debts/:roommateId', async function(req, res) {
     res.send('Get all debts for roommate ' + req.params['roommateId'] + 'from house ' + req.params['houseId']);
 });
 
-router.get('/:houseId/debts/:userRoommateId/:otherRoommateId', function(req, res) {
+router.get('/:houseId/debts/:userRoommateId/:otherRoommateId', async function(req, res) {
     res.send('Get all debts for roommates ' + req.params['userRoommateId'] + ' and ' + req.params['otherRoommateId'] + ' from house ' + req.params['houseId']);
 });
 
