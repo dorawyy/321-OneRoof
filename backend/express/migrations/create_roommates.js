73b7839dc008aa7d856e79cc3c464fbd65ab16eb
Jackson Dagger <JacksonDDagger@gmail.com>
2020-10-26 20:57:58 -0700

    setup login (maybe)

--- backend/express/migrations/create_roommates.js
+++ backend/express/migrations/create_roommates.js
@@ -1,9 +1,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('roommates', (table) => {
         table.increments('roommate_id').notNullable();
-        table.string('roommate_name').notNullable();
+        table.string('roommate_name');
+        table.string('roommate_uid');
         table.boolean('roommate_is_site_admin').default(false);
-        table.integer('roommate_house').unsigned().notNullable();
+        table.integer('roommate_house').unsigned();
         table.foreign('roommate_house').references('houses.house_id');
     });
 };
