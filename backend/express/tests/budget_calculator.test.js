29b0bdc1073cf882c818077ae12d62e0d5db502c
Jackson Dagger <JacksonDDagger@gmail.com>
2020-11-23 10:10:57 -0800

    t distribution fixed for small degrees of freedom

---
+++ backend/express/tests/budget_calculator.test.js
@@ -0,0 +1,15 @@
+const budgetCalculator = require("../budget");
+
+test('Simple list, far under', () => {
+    var purchases = [50, 20, 10, 5];
+    var limit = 1500;
+    expect(budgetCalculator.budgetPredictionFromList(purchases, limit).likelihood)
+    .toBeCloseTo(0, 4);
+});
+
+// test('Simple list, slightly under', () => {
+//     var purchases = [100, 200, 145, 355, 100];
+//     var limit = 1000;
+//     expect(budgetCalculator.budgetPredictionFromList(purchases, limit).likelihood)
+//     .toBeCloseTo(0.3476, 4);
+//   });
