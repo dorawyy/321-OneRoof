f8418acac16e45a084412a9c76c19e0dad694cc6
Sam Schweigel <s.schweigel@gmail.com>
2020-12-02 12:03:13 -0800

    Fix sqlite support for testing

--- backend/express/migrations/A003_add_foreign_key_to_houses.js
+++ backend/express/migrations/A003_add_foreign_key_to_houses.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
     return knex.schema.table("houses", function (table) {
-        table.integer("house_admin").unsigned().notNullable();
+        table.integer("house_admin").unsigned();
         table.foreign("house_admin").references("roommates.roommate_id");
     });
 };
