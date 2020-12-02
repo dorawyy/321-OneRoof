9c3c12897c44bac26f8e10de2d1aa2bba9f1bdc3
Sam Schweigel <s.schweigel@gmail.com>
2020-12-02 15:53:56 -0800

    Make travis run the budget integration tests

---
+++ backend/express/knexfile_travis.js
@@ -0,0 +1,16 @@
+module.exports = {
+  test: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: 'dev.sqlite3',
+      database: 'dev_oneroof',
+      migrations: {
+          directory: __dirname + '/migrations'
+      },
+      seeds: {
+          directory: __dirname + '/seeds'
+      },
+    }
+  },
+};
